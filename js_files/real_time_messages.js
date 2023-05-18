23f92679808003705a403f0d7bc171612e27b427
beatrizremondes <115415537+beatrizremondes@users.noreply.github.com>
2023-05-18 14:22:58 +0100

    Implement update real time messages

--- js_files/real_time_messages.js
+++ js_files/real_time_messages.js
@@ -1,92 +1,14 @@
-// Get the form element and chat message container
-const form = document.getElementById('chat-form');
-const chatMessages = document.getElementById('chat_messages');
+$(document).ready(function() {
+  const maxHeight = 5000000000;
+  $('#chat-box').scrollTop(maxHeight);
 
-// Get the ticket ID from the URL
-const urlParams = new URLSearchParams(window.location.search);
-const ticketId = urlParams.get('ticket_id');
-
-// Set up a function to handle new messages
-function handleNewMessage(message) {
-  // Create a new div element with the class "message"
-  const newMessage = document.createElement('div');
-  newMessage.classList.add('message');
-  newMessage.innerHTML = message;
-
-  // Append the new div element to the chatMessages container
-  chatMessages.appendChild(newMessage);
-}
-
-// Set up a function to get new messages from the server
-function getNewMessages() {
-  // Create a new XMLHttpRequest object
-  const xhr = new XMLHttpRequest();
-
-  // Set up the xhr object to send a GET request to the server
-  xhr.open('GET', '../messages/messages.php?ticket_id=' + encodeURIComponent(ticketId), true);
-
-  // Set up the xhr object to handle the response from the server
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-      if (xhr.status === 200) {
-        // If the server returns a "success" response, parse the response as JSON
-        const response = JSON.parse(xhr.responseText);
-
-        // Iterate over each new message in the response and handle it
-        response.forEach((message) => {
-          handleNewMessage(message);
-        });
-      } else {
-        // If the server returns an error response, log the error
-        console.error('Error getting new messages:', xhr.responseText);
+  $.ajax({
+    type: 'GET',
+    url: '../messages/messages.php',
+    success: function(response) {
+      if (response == 1) {
+        document.location.reload(true);
       }
     }
-  };
-
-  // Send the GET request
-  xhr.send();
-}
-
-// Call getNewMessages() to start the process
-getNewMessages();
-
-// Add an event listener to the form that listens for the "submit" event
-form.addEventListener('submit', (event) => {
-  // Prevent the form from submitting normally
-  event.preventDefault();
-
-  // Get the message input element and its value
-  const messageInput = document.getElementById('message_text');
-  const message = messageInput.value;
-
-  // Create a new XMLHttpRequest object
-  const xhr = new XMLHttpRequest();
-
-  // Set up the xhr object to send a POST request to the server
-  xhr.open('POST', '../messages/messages.php?ticket_id=' + encodeURIComponent(ticketId), true);
-  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
-  // Set up the xhr object to handle the response from the server
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-      if (xhr.status === 200) {
-        // If the server returns a "success" response, reset the message input and handle the new message
-        messageInput.value = '';
-        handleNewMessage(message);
-      } else {
-        // If the server returns an error response, log the error
-        console.error('Error submitting message:', xhr.responseText);
-      }
-    }
-  }
-
-  // Send the POST request with the message as the data
-  xhr.send('new_message=' + encodeURIComponent(message));
+  });
 });
-
-
-
-
-
-
-
--- messages/messages.php
+++ messages/messages.php
@@ -2,50 +2,91 @@
 session_start();
 ob_start();
 $conn = new PDO('sqlite:../database.db');
-function add_paragraphs($input) {
-  $paragraphs = explode("\n", $input);
+
+function fetch_messages_by_ticket_id($conn) {
+  $ticket_id = $_GET['ticket_id'];
+
+  $stmt = $conn->prepare('SELECT * FROM Messages WHERE ticket_id=?');
+  $stmt->bindParam(1, $ticket_id);
+  $stmt->execute();
+
+  $messages = $stmt->fetchAll();
+
+  return $messages;
+}
+
+function fetch_username_by_id($conn, $user_id) {
+  $stmt = $conn->prepare('SELECT username FROM Users WHERE user_id = ' . $user_id);
+  $stmt->execute();
+  $username = $stmt->fetch()[0];
+
+  return $username;
+}
+
+function fetch_all_messages($conn) {
+  // Get all mesages assigned to this ticket
+  $messages = fetch_messages_by_ticket_id($conn);
+
+  // Get current user id <=> sender id
+  $current_user_id = $_SESSION['user_id'];
+
   $output = '';
-  foreach ($paragraphs as $paragraph) {
-   $output .= "<p>$paragraph</p>";
+  foreach($messages as $message) {
+    if($message['sender_id'] == $current_user_id){
+      $div_name = 'my_message';
+    }
+    else{
+      $div_name = 'received_message';
+    }
+
+    // TODO: Make sure the username is right
+    $current_username = fetch_username_by_id($conn, $current_user_id);
+
+    // Display all the messages associated to this ticket
+    $output .= '<div id = "' . $div_name . '" ><p id = "time_of_message">' . $current_username . '<small> ' .  $message['time_of_message'] . '</small></p><p>' . $message['content'] . '</p></div>';
   }
   return $output;
 }
+
 $stmt = $conn->prepare('SELECT client_id from Tickets WHERE ticket_id = ?');
 $verification_ticket_id = $_GET['ticket_id'];
 $stmt->bindParam(1,$verification_ticket_id);
 $stmt->execute();
+
 $client_id = $stmt->fetchColumn();
 $stmt = $conn->prepare('SELECT ticket_department_id FROM Tickets WHERE ticket_id = ?');
 $stmt->bindParam(1,$verification_ticket_id);
 $stmt->execute();
+
 $ticket_department_id = $stmt->fetchColumn();
 $staff_user_id = $_SESSION['user_id'];
 $stmt = $conn->prepare('SELECT department_id FROM Admins WHERE admin_id = ?');
 $stmt->bindParam(1,$staff_user_id);
 $stmt->execute();
+
 $verification_ticket_department_id_admins = $stmt->fetchColumn();
 $stmt = $conn->prepare('SELECT department_id FROM Agents WHERE agent_id = ?');
 $stmt->bindParam(1,$staff_user_id);
 $stmt->execute();
+
 $verification_ticket_department_id_agents = $stmt->fetchColumn();
 
-if((isset($_SESSION['username']) && $_SESSION['user_id'] == $client_id) || ($_SESSION['usertype'] == 'admin' && $_SESSION['admin_type'] == 'main admin') || ($_SESSION['usertype'] == 'admin' && $_SESSION['admin_type'] == 'local admin' && $ticket_department_id == $verification_ticket_department_id_admins) || ($_SESSION['usertype'] == 'admin' && $_SESSION['admin_type'] == 'main admin') || ($_SESSION['usertype'] == 'agent' && $ticket_department_id == $verification_ticket_department_id_agents)){
- if(isset($_GET['ticket_id'])){
-
+if((isset($_SESSION['username']) && $_SESSION['user_id'] == $client_id) || ($_SESSION['usertype'] == 'admin' && $_SESSION['admin_type'] == 'main admin') || ($_SESSION['usertype'] == 'admin' && $_SESSION['admin_type'] == 'local admin' && $ticket_department_id == $verification_ticket_department_id_admins) || ($_SESSION['usertype'] == 'admin' && $_SESSION['admin_type'] == 'main admin') || ($_SESSION['usertype'] == 'agent' && $ticket_department_id == $verification_ticket_department_id_agents)) {
+ if(isset($_GET['ticket_id'])) {
 ?>
 
 <!DOCTYPE html>
 <html>
- <head>
- <title>Messages</title>
- <link rel="stylesheet" href="../style.css">
+<head>
+  <title>Messages</title>
+  <link rel="stylesheet" href="../style.css">
 </head>
 <body>
-<header class = "header1">
- <h1>Trouble Tickets<h1>
- <h2>Here to help you solve all your tech problems!</h2>
- <a href="http://localhost:9000/main.php" class="home-button"><img src="../images/home_icon.png" alt="Home"></a>
-</header>
+  <header class = "header1">
+    <h1>Trouble Tickets<h1>
+    <h2>Here to help you solve all your tech problems!</h2>
+    <a href="http://localhost:9000/main.php" class="home-button"><img src="../images/home_icon.png" alt="Home"></a>
+  </header>
 <nav id="main_menu">
     <ul>
       <li>
@@ -55,9 +96,10 @@ if((isset($_SESSION['username']) && $_SESSION['user_id'] == $client_id) || ($_SE
           <li>
             <span>Edit Profile</span>
             <ul>
-              <li><a href="http://localhost:9000/profiles/change-username.php">Change Username</a></li>
-              <li><a href="http://localhost:9000/profiles/change-password.php">Change Password</a></li>
-              <li><a href="http://localhost:9000/background/logout.php">Logout</a></li>
+             <li><a href="#" onclick="showModal('change_username')">Change Username</a></li>
+             <li><a href="#" onclick="showModal('change_email')">Change Email</a></li>
+             <li><a href="#" onclick="showModal('change_password')">Change Password</a></li>
+             <li><a href="http://localhost:9000/background/logout.php">Logout</a></li>
             </ul>
           </li>
         </ul>
@@ -78,7 +120,7 @@ if((isset($_SESSION['username']) && $_SESSION['user_id'] == $client_id) || ($_SE
           <li><a href="http://localhost:9000/departments/web-development.php">Web Development</a></li>
           <li><a href="http://localhost:9000/departments/app-development.php">App Development</a></li>
           <li><a href="http://localhost:9000/departments/network-support.php">Network Support</a></li>
-          <li><a href="http://localhost:9000/departments/costomer-service.php">Costomer Service</a></li>
+          <li><a href="http://localhost:9000/departments/customer-service.php">Customer Service</a></li>
           <li><a href="http://localhost:9000/departments/security-issues.php">Security Issues</a></li>
         </ul>
       </li>
@@ -99,7 +141,7 @@ if((isset($_SESSION['username']) && $_SESSION['user_id'] == $client_id) || ($_SE
        </ul>
       </li>
       <?php
-       if($_SESSION['usertype'] == "agent" || $_SESSION['usertype'] == "admin"){
+       if($_SESSION['usertype'] == "agent" || $_SESSION['usertype'] == "admin") {
         ?>
         <li>
          <span>Staff</span>
@@ -110,8 +152,8 @@ if((isset($_SESSION['username']) && $_SESSION['user_id'] == $client_id) || ($_SE
          </ul>
         </li>
       <?php
-       }
-       if($_SESSION['usertype'] == "admin"){
+        }
+       if($_SESSION['usertype'] == "admin") {
       ?>
        <li>
         <span>Management</span>
@@ -131,65 +173,49 @@ if((isset($_SESSION['username']) && $_SESSION['user_id'] == $client_id) || ($_SE
           <li><a href="http://localhost:9000/management/requests.php">Requests & Complaints Inbox</a></li>
         </ul>    
        </li>
-      <?php
+       <?php
        }
       ?>
     </ul>
   </nav>  
   <div id="chat-box-wrapper">
-  <div id="chat-box">
-    <div id="chat_messages">
-      <?php
-       $stmt = $conn->prepare('SELECT * FROM Messages WHERE ticket_id = ?');
-       $ticket_id = $_GET['ticket_id'];
-       $stmt->bindParam(1,$ticket_id);
-       $stmt->execute();
-       $messages = $stmt->fetchAll();
-       foreach($messages as $message){
-        if($message['sender_id'] == $_SESSION['user_id']){
-         $div_name = 'my_message';
-        }
-        else{
-         $div_name = 'received_message';
-        }
-        echo '<div id = "' . $div_name . '" >';
-         echo '<p>' . $message['content'] . '</p>';
-         echo '<p id = "time_of_message">' .$message['time_of_message'] . '</p>';
-        echo '</div>';
-       }
+    <div id="chat-box">
+      <?php 
+      echo fetch_all_messages($conn);
       ?>
     </div>
   </div>
-</div>
- <form id="chat-form" method = "POST">
-      <textarea class = "message-text" id = "message_text" name = "new_message"></textarea>
-      <button type="submit" id = "send_button">Send</button>
-  </form>
+<form method="POST" id="chat-form">
+       <textarea id="message_text" name="new_message" placeholder="Enter message here"></textarea>
+       <input type="submit" id="send_button" value="Send"/>
+</form>
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
- $new_message = $_POST['new_message'];
- $stmt = $conn->prepare('INSERT INTO Messages(receiver_id,sender_id,content,ticket_id,time_of_message) VALUES(?,?,?,?,?)');
- $sender_id = $_SESSION['user_id'];
- $receiver_id = $client_id;
- $content = add_paragraphs($new_message);
- $ticket_id = $_GET['ticket_id'];
- $time_of_message = date('d/m/Y H:i');
- $display_time_of_message = date('H:i');
- $stmt->bindParam(1,$receiver_id);
- $stmt->bindParam(2,$sender_id);
- $stmt->bindParam(3,$content);
- $stmt->bindParam(4,$ticket_id);
- $stmt->bindParam(5,$time_of_message);
- $stmt->execute();
- ob_clean();
- $refresh_url = 'messages.php?ticket_id=' . $ticket_id;
- header('Location:' . $refresh_url);
+  $stmt = $conn->prepare('INSERT INTO Messages(receiver_id,sender_id,content,ticket_id,time_of_message) VALUES(?,?,?,?,?)');
+  
+  $new_message = $_POST['new_message'];
+  $sender_id = $_SESSION['user_id'];
+  $receiver_id = $client_id;
+  $ticket_id = $_GET['ticket_id'];
+  $time_of_message = date('d/m/Y H:i');
+
+  $stmt->bindParam(1,$receiver_id);
+  $stmt->bindParam(2,$sender_id);
+  $stmt->bindParam(3,$new_message);
+  $stmt->bindParam(4,$ticket_id);
+  $stmt->bindParam(5,$time_of_message);
+  $stmt->execute();
+
+  ob_clean();
+  $refresh_url = 'messages.php?ticket_id=' . $ticket_id;
+  header('Location:' . $refresh_url);
 }
 ?>
 <footer>
  <p>© Copyright 2021-2023 IT Ticket</p>
  <p><a href = "http://localhost:9000/privacy/privacy_policy.php">Privacy Policy</a></p>
 </footer>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
 <script src="../js_files/click.js"></script>
 <script src="../js_files/close_ticket_confirmation.js"></script>
 <script src="../js_files/increasing-textarea-size.js"></script>
@@ -198,6 +224,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 </body>
 </html>
 <?php
- }
+  }
 }
 ?>
